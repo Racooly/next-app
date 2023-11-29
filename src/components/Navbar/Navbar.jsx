391dca3da191f0a922a990c3ffcf345895c0efec
Racooly <sandroiremadze14@gmail.com>
2023-11-29 18:46:20 +0400

    routing added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,30 +1,37 @@
 import React from 'react'
 import styles from './navbar.module.css'
+import Link from 'next/link'
 function Navbar() {
   const links = [
     {
       id: 1,
-       title: 'Home'
+      title: 'Home',
+      url: '/'
     },
     {
       id:2,
-      title: 'Portfolio'
+      title: 'Portfolio',
+      url: '/portfolio'
     },
     {
       id:3,
-      title: 'blog'
+      title: 'blog',
+      url: '/blog'
     },
     {
       id:4,
-      title: 'about'
+      title: 'about',
+      url: '/about'
     },
     {
       id:5,
-      title: 'Contact'
+      title: 'Contact',
+      url: '/contact'
     },
     {
       id:6,
-      title: 'Dashboard'
+      title: 'Dashboard',
+      url: '/dashboard'
     }
   ]
 
@@ -34,10 +41,9 @@ function Navbar() {
       <div>Main</div>
       <div className={styles.links}>
         {links.map((link) => (
-          <div>{link.title}</div>
+          <Link href={link.url}>{link.title}</Link>
         ))}
       </div>
-    
     </div>
   )
 }
