815a02cfceae4e3b6e8e785fd25d1a0af48beb07
Racooly <sandroiremadze14@gmail.com>
2023-12-01 13:35:12 +0400

    app_UI added

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,22 @@
 
-import Navbar from '@/components/Navbar/Navbar'
 import styles from './page.module.css'
-import Footer from '@/components/Footer/Footer'
+import Image from 'next/image'
+import hero from 'public/hero.png'
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      
+    <main className={styles.container}>
+      <div className={styles.item}>
+        <h1 className={styles.title}>Better design dor your digital products.</h1>
+        <p className={styles.desc}>
+          Turning your Idea into Reality. We bring together the teams from the
+          global tech industry.
+        </p>
+        <button className={styles.button}>See Out Works</button>
+      </div>
+      <div className={styles.item}>
+        <Image src={hero} alt="hero" className={styles.img} />
+      </div>
     </main>
   )
 }
